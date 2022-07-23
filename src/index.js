787a1480349ade575f66bb15bede60cfafd2a58f
muellerviana <muellerviana@gmail.com>
2022-07-23 17:25:39 -0300

    better understand

--- src/index.js
+++ src/index.js
@@ -4,18 +4,40 @@ import './index.css';
 
 
 class Square extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: null,
+    };
+  }
+
   render() {
     return (
-      <button className="square">
-        {this.props.value}
+      <button
+        className="square"
+        onClick={() => this.setState({value: 'X'})}
+      >
+        {this.state.value}
       </button>
     );
   }
 }
 
 class Board extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null),
+    };
+  }
+
   renderSquare(i) {
-    return <Square value={i} />;
+    return (
+      <Square 
+        value={this.state.squares[i]}
+        onClick={() => this.handleClick(i)}
+      />
+    );
   }
 
   render() {
@@ -64,3 +86,11 @@ class Game extends React.Component {
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<Game />);
+
+
+
+// [
+//   'O', null, 'X',
+//   'X', 'X', 'O',
+//   'O', null, null,           tipe this to find where I am
+// ]
